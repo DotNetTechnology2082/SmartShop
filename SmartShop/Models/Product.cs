14a7460a98e9d76c70dbefd033a4fd9d64a8bf24
Nabin Kandel <nkandel208@gmail.com>
2026-05-12 19:34:14 -0700

    Add project files.

---
+++ SmartShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+        public string Description { get; set; } = "Product";
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, 100, ErrorMessage = "Price must be between 0 and 100")]
+        public decimal Price { get; set; }
+        public string? Rate { get; set; } 
+        public int CategoryId { get; set; }
+    }
+}
